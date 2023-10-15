a67dada8674cf27e7ca2241070f2b785398e0832
Alisa <Alisa.dragun@mail.ru>
2023-10-15 18:35:12 +0200

    done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        return this.middle = Math.round((this.left + this.right) / 2);
     }
 
     lower() {
-
+        this.right = this.middle;
     }
 
     greater() {
-
+        this.left = this.middle;
     }
 }
 
